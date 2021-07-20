59af620e694f114b6ebfcebfb74a2c5ac0a5b94a
Martín Calderón <martincalderon92@outlook.com>
2021-07-20 18:36:13 -0300

    Body pass to App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
-import Body from './components/Body';
+import FormCard from './components/FormCard';
+import TasksTable from './components/TasksTable';
 
 export default function App() {
+	const useStyles = makeStyles(() => ({
+		body: {
+			display: 'flex',
+			justifyContent: 'space-around',
+			margin: '20px 20px 0px 0px',
+		},
+	}));
+
+	const classes = useStyles();
+
+	const [refreshTasks, setRefreshTasks] = useState(0);
+	const [getTask, setGetTask] = useState(null);
+
+	const onRefreshTasks = () => {
+		setRefreshTasks(refreshTasks + 1);
+	};
+
+	const onGetTask = (task) => {
+		setGetTask(task);
+	};
+
 	return (
 		<div className="App">
 			<Header />
-			<Body />
+			<div className={classes.body}>
+				<FormCard handleRefreshTasks={onRefreshTasks} getTask={getTask} />
+				<TasksTable refreshTasks={refreshTasks} handleGetTask={onGetTask} />
+			</div>
 		</div>
 	);
 }
